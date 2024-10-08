3fddbcb157ee9debee3c9d64ae843c5fd80952a7
jq-zhang22 <113609166+jq-zhang22@users.noreply.github.com>
2024-10-08 22:54:48 +0800

    Add files via upload

---
+++ build/CMakeFiles/mesh_main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mesh_main.dir/src/main.cpp.obj"
+  "CMakeFiles/mesh_main.dir/src/main.cpp.obj.d"
+  "CMakeFiles/mesh_main.dir/src/mesh.cpp.obj"
+  "CMakeFiles/mesh_main.dir/src/mesh.cpp.obj.d"
+  "libmesh_main.dll.a"
+  "mesh_main.exe"
+  "mesh_main.exe.manifest"
+  "mesh_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mesh_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
